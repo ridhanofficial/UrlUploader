a2e280d4c62774590ca74ef65505ebe6a1325756
RIDHAN R <158157138+ridhanofficial@users.noreply.github.com>
2025-01-10 16:21:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7643545087:AAHMwI-sn16xD1bQuUBCP-C-kQNA
 SESSION_STRING = os.environ.get("SESSION_STRING", "BQFaeV8AjMhWpjCt50an2xxDSWyDQkw0kBXvyZiygxlF0aEgU8eULldJNS2ZeO1UyCcRohRotHaZWHDTl6Wo2Gl2eu8hn5y6AK8sYLaUuGg0GWYZ0Aahz1jWLbiLftN0AKQti8o6oY1cIkCLbxnRkXJjcg8zCh57WlfGDLmulTfIEGvjX6l3gJCEnkL5vZNSOk6-wE-v-tZC3KtHAWU2Wb3XZgbBQ4Zj_w8b3waRUEk1DE61N0hD5Q4x0Sb_7hZK-LYD7GBCM7xMGsF_3jJf93lvrYdeHuLJ9jw6fiUwn6sbaCS1etW514PNJd8i1wstGJW1U2YKSMeaUkuYr_WXa6BHAEbn8QAAAAHHlz3_AQ")  # Replace with actual SESSION STRING
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5825793375"))  # Replace with actual owner ID
-
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "@RSforeverBots")
 # File size limit (4GB)
 MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4GB in bytes
 DOWNLOAD_LOCATION = "./DOWNLOADS"  # File / video download location
